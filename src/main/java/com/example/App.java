e126b74361acfdde0bcfdc800d4f698318faf25d
iYaffa <yafawx@gmail.com>
2025-08-16 20:19:18 +0300

    final version

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,6 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,65 +13,40 @@ public class App {
     static String url = "jdbc:mysql://localhost:3306/TAS";
     static String user = "yaffa";
     static String password = "yaffa";
-    static HashMap<String, Record> records = new HashMap<>(); //to keep track of the records
-    static Connection conn;
+    static HashMap<String, Record> records = new HashMap<>();
 
     public static void main(String[] args) {
 
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            String query = "SELECT clock_id, employee_id, transaction_symbol, transaction_time FROM clock_data      WHERE posted='N'";
-            Statement s = conn.createStatement();
-            ResultSet res = s.executeQuery(query);
-            Record rec;
-            while (res.next()) {
-                int clock_id = res.getInt("clock_id");
-                String employee_id = res.getString("employee_id");
-                String transaction_symbol = res.getString("transaction_symbol");
-                Timestamp transaction_time = res.getTimestamp("transaction_time");
-                boolean isStart = defineSymbol(transaction_symbol, clock_id);
-                if (!records.containsKey(employee_id)) {
-                    rec = new Record(employee_id, null, null);
-                    records.put(employee_id, rec);
-                } else {
-                    rec = records.get(employee_id);
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+
+            String query = "SELECT cd.clock_id, cd.employee_id, cd.transaction_symbol, cd.transaction_time, "
+                    + "CASE WHEN cs.clock_start_symbol = cd.transaction_symbol THEN 'start' "
+                    + "WHEN cs.clock_end_symbol = cd.transaction_symbol THEN 'end' ELSE 'UNKNOWN' END AS symbol_type "
+                    + "FROM clock_data cd "
+                    + "JOIN clock_symbol cs ON cd.clock_id = cs.clock_terminal_id "
+                    + "WHERE cd.posted = 'N'";
+
+            try (Statement stmt = conn.createStatement(); ResultSet res = stmt.executeQuery(query)) {
+
+                while (res.next()) {
+                    String employee_id = res.getString("employee_id");
+                    Timestamp transaction_time = res.getTimestamp("transaction_time");
+                    boolean isStart = "start".equals(res.getString("symbol_type"));
+
+                    Record rec = records.computeIfAbsent(employee_id, k -> new Record(employee_id, null, null));
+
+                    if (isStart) {
+                        rec.setStart_time(transaction_time);
+                    } else {
+                        rec.setEnd_time(transaction_time);
+                    }
+
+                    System.out.println(rec);
                 }
-
-                if (isStart) {
-                    rec.setStart_time(transaction_time);
-                } else {
-                    rec.setEnd_time(transaction_time);
-                }
-                System.out.println(rec);
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    //defining whether the symbol is start or end based on the clock id
-    private static boolean defineSymbol(String transaction_symbol, int clock_id) throws Exception {
-        try {
-            String query = "SELECT CASE  WHEN clock_start_symbol  = ? THEN 'start'   WHEN clock_end_symbol = ? THEN 'end' END AS symbol_type FROM  clock_symbol WHERE clock_terminal_id= ?;";
-            PreparedStatement prep = conn.prepareStatement(query);
-            prep.setString(1, transaction_symbol);
-            prep.setString(2, transaction_symbol);
-            prep.setInt(3, clock_id);
-            ResultSet res = prep.executeQuery();
-
-            if (res.next()) {
-                return "start".equals(res.getString("symbol_type"));
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        throw new Exception("Mismatch in symbol");
     }
-
 }
