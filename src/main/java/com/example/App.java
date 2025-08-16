824102d14d9c19c5ef134c69a025e122e30731d8
iYaffa <yafawx@gmail.com>
2025-08-16 20:36:34 +0300

    final version

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -19,21 +19,21 @@ public class App {
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
 
-            String query = "SELECT cd.clock_id, cd.employee_id, cd.transaction_symbol, cd.transaction_time, "
-                    + "CASE WHEN cs.clock_start_symbol = cd.transaction_symbol THEN 'start' "
-                    + "WHEN cs.clock_end_symbol = cd.transaction_symbol THEN 'end' ELSE 'UNKNOWN' END AS symbol_type "
-                    + "FROM clock_data cd "
-                    + "JOIN clock_symbol cs ON cd.clock_id = cs.clock_terminal_id "
-                    + "WHERE cd.posted = 'N'";
+            String query = "SELECT clock_id, employee_id, transaction_symbol, transaction_time, "
+                    + "CASE WHEN transaction_symbol = (SELECT clock_start_symbol FROM clock_symbol WHERE clock_terminal_id = clock_data.clock_id AND transaction_type='CHECK') THEN 'start' "
+                    + "WHEN transaction_symbol = (SELECT clock_end_symbol FROM clock_symbol WHERE clock_terminal_id = clock_data.clock_id  AND transaction_type='CHECK') THEN 'end' "
+                    + "ELSE 'UNKNOWN' END AS symbol_type "
+                    + "FROM clock_data WHERE posted = 'N'";
 
             try (Statement stmt = conn.createStatement(); ResultSet res = stmt.executeQuery(query)) {
 
+                Record rec;
                 while (res.next()) {
                     String employee_id = res.getString("employee_id");
                     Timestamp transaction_time = res.getTimestamp("transaction_time");
                     boolean isStart = "start".equals(res.getString("symbol_type"));
 
-                    Record rec = records.computeIfAbsent(employee_id, k -> new Record(employee_id, null, null));
+                    rec = records.computeIfAbsent(employee_id, k -> new Record(employee_id, null, null));
 
                     if (isStart) {
                         rec.setStart_time(transaction_time);
